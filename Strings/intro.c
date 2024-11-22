c36d995e0c5611729507da9f3b028278f17aaed4
Abhilash107 <abhilash00003108884@gmail.com>
2024-11-22 22:26:30 +0530

    checking

--- Strings/intro.c
+++ Strings/intro.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+
 int main(){
     char str[40];
     int cnt = 0;
     printf("Enter your name: ");
     fgets(str, sizeof(str), stdin);
     // Without &
+
     printf("%s", str);
     puts(str); // puts() for displaying strings
+
     // Count length
     for(int i = 0; str[i] != '\0';i++){
         cnt++;
     }
     printf("Length of str: %d\n", cnt - 1);
     //printf("Length of str: %d", (int)strlen(str)); don't use when fgets is used
+
+    char s[] = {"abc"};
+    for(int i = 0;i < strlen(s);i++){
+        printf("%c\t", s[i]);
+    }
 
 }
