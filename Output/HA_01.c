fbd41b4a89f5e63555c05ed44234a52223537961
Abhilash107 <abhilash00003108884@gmail.com>
2024-11-28 19:32:15 +0530

    output

--- Output/HA_01.c
+++ Output/HA_01.c
@@ -41,13 +41,42 @@
 // return 0;
 // }
 
-#define x 10+10
-#include <stdio.h>
-#define scanf "%s Practical C Y-N"
+// #define x 10+10
+// #include <stdio.h>
+// #define scanf "%s Practical C Y-N"
+// int main(){
+// int a=x*x;
+// printf("%d\n",a);
+// printf(scanf, scanf);
+// return 0;
+// }
+
+
+// #define SUM 100
+// #include <stdio.h>
+// int main(){
+// SUM=200;
+// printf("%d", SUM);
+// return 0;
+// }
+// #define x 97
+// #define x 65
+// #include <stdio.h>
+// int main(){
+// printf("x+x=%d\n",x+x);
+// printf("%d %c\n",x,x);
+// printf("%d",x+5+x/5);
+// return 0;
+// }
+
+#include <stdio.h>// ## token pasting
+#define con(m, a) m##a
 int main(){
-int a=x*x;
-printf("%d\n",a);
-printf(scanf, scanf);
+int con(x,3)=120;
+printf("Concat=%d\n",x3);// 120
+con(x,3)=x3+con(12,45);// 120 + 1245 ==> 1365
+printf("Concat=%d\n",x3);
+printf("con(x,3)=%d\n",con(x,3));
 return 0;
 }
 
