71c1fdaf6a2528937c01d8279a7355b86d9d7187
Abhilash107 <abhilash00003108884@gmail.com>
2024-11-28 19:13:39 +0530

    outputs

---
+++ Output/HA_01.c
@@ -0,0 +1,61 @@
+// #include<stdio.h>
+// #define print printf("Symbolic Constants\n")
+// int main(){
+// print;
+// print;
+// return 0;
+// }  
+
+
+// #include <stdio.h>
+// #define AH 65
+// int main(){
+// printf("%d\n",AH);
+// #define AH 41
+// printf("%d\n",AH);
+// return 0;
+// }
+
+ //#include<stdio.h>
+// #define MMM 10+20
+// int main(){
+// printf("x=%d\n",MMM*MMM);
+// return 0;
+// }
+
+// #define MAX (a>b ? a : b)
+// int main(){
+// int a=77,b=67;
+// int c=MAX;
+// printf("MAX=%d\n",c);
+// printf("%s","MAX");
+// return 0;
+// }
+
+
+// #define x (10+10)
+// #include <stdio.h>
+// int main(){
+// int a=x*x;
+// printf("%d",a);
+// return 0;
+// }
+
+#define x 10+10
+#include <stdio.h>
+#define scanf "%s Practical C Y-N"
+int main(){
+int a=x*x;
+printf("%d\n",a);
+printf(scanf, scanf);
+return 0;
+}
+
+
+
+
+
+
+
+
+
