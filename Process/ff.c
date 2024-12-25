0af557a15ff3e4e6ad04e31a693134f96fbacc98
Abhilash107 <abhilash00003108884@gmail.com>
2024-12-25 20:46:21 +0530

    Comment out process ID output in ff.c

--- Process/ff.c
+++ Process/ff.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 int main(void) {
-    printf("I am process %ld\n", (long)getpid());
+    //printf("I am process %ld\n", (long)getpid());
     printf("My parent is %ld\n", (long)getppid());
     return 0;
 }
